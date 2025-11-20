cb591a5938a5dd15e4c792371b47b9cbcb0f889f
Raphides <raphaelmendesdasilva@gmail.com>
2025-11-20 20:30:28 -0300

    Componente de input de texto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TextInputFieldComponent } from './components/text-input-field/text-input-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TextInputFieldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/text-input-field/text-input-field.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-input-field',
+  templateUrl: './text-input-field.component.html',
+  styleUrls: ['./text-input-field.component.css']
+})
+export class TextInputFieldComponent implements OnInit {
+  @Input() component_id: number = 0;
+  @Input() label: string = "Input de texto";
+  @Input() placeholder: string = "Digite algo";
+  // TODO: check if id is integer.
+  input_type: string = "text";
+  @Input("is-password") set setHiddenInput(value: boolean) {
+    if (value) {
+      this.input_type = "password";
+    }
+  }
+  
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
